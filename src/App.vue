bc83880c18f46890b9abc806b08a2f3fd62e2c8e
Maciej Sznurawa <msznurawa@gmail.com>
2020-04-23 09:41:35 +0200

    Add global styles files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">@</router-link> |
-      <router-link to="/mountains">@</router-link> |
-      <router-link to="/winter">@</router-link> |
-      <router-link to="/flora">@</router-link> |
-      <router-link to="/fauna">@</router-link> |
+      <router-link to="/">@</router-link>
+      <router-link to="/mountains">@</router-link>
+      <router-link to="/winter">@</router-link>
+      <router-link to="/flora">@</router-link>
+      <router-link to="/fauna">@</router-link>
       <router-link to="/routes">@</router-link>
     </div>
     <router-view />
@@ -14,10 +14,6 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
@@ -25,12 +21,17 @@
   position: fixed;
   bottom: 0;
   left: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
   width: 100%;
-  padding: 30px 0;
+  padding: 20px 0;
 
   a {
     font-weight: bold;
     color: #2c3e50;
+    padding: 0 10px;
+    text-decoration: none;
 
     &.router-link-exact-active {
       color: #42b983;
